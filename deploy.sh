00fc6113615be161276b507761b70f0ce6a2007a
ishu22g <ishu22g@gmail.com>
2020-04-06 23:47:01 -0700

    Adding final changes

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t ishu22g/multi-client:latest -t ishu22g/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t ishu22g/multi-server:latest -t ishu22g/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t ishu22g/multi-worker:latest -t ishu22g/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push ishu22g/multi-client:latest
+docker push ishu22g/multi-server:latest
+docker push ishu22g/multi-worker:latest
+
+docker push ishu22g/multi-client:$SHA
+docker push ishu22g/multi-server:$SHA
+docker push ishu22g/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=ishu22g/multi-client:$SHA
+kubectl set image deployments/server-deployment server=ishu22g/multi-server:$SHA
+kubectl set image deployments/server-deployment server=ishu22g/multi-worker:$SHA
